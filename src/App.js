50a32cb5afaa85b9f9c4fe0e403c94a2e1a58056
daniel <kimgooneya@gmail.com>
2022-12-04 23:26:45 +0900

    6.4 cleanup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import React, { useEffect } from "react";
 
+function Hello(){
+
+  useEffect(()=>{
+    console.log("hi :)");
+    return () => console.log("byte :(");
+  },[]);
+  return <h1>Hello</h1>
+}
+
 function App() {
+  const [showing, setShowing] = React.useState(false);
+  const onClick = () => setShowing(prev => !prev);
   return (
     <div>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing? "Hide": "Show"}</button>
     </div>
   );
 }
