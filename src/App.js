03a0f2ed41593ca98c42dec50fe1bdc0c07da514
daniel <kimgooneya@gmail.com>
2022-12-06 00:07:16 +0900

    7.0 To Do List part one

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setTodo("");
+  }
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <input
+        onChange={onChange}
+        value={toDo}
+        type="text"
+        placeholder="Write your to do..." />
+      <button onClick={onSubmit}>Add To do</button>
+    </div>
+
+  );
 }
 
 export default App;
