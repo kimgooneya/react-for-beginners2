a3123c6b8e4b87f7b74ca9888e5953eea4b8590f
daniel <kimgooneya@gmail.com>
2022-12-15 01:14:56 +0900

    7.6 homework

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,22 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail(){
     const {id} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [title, setTitle] = useState("");
+    const [img, setImg] = useState("");
+    const [summary, setSummary] = useState("");
+    const [genres, setGenres]= useState([]);
 
     const getMovie = async() =>{
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
-        console.log(json);
+        setLoading(false);
+        setGenres(json.data.movie.genres);
+        setTitle(json.data.movie.title);
+        setImg(json.data.movie.medium_cover_image);
+        setSummary(json.data.movie.summary);
     };
-    useEffect(()=>{
-        getMovie();
-    },[]);
+    
+    // eslint-disable-next-line
+    useEffect(_=>{ getMovie()},[]);
+
     return (
-    <h1>Detail</h1>
-    
-    
+        <div>
+            <h1>Detail</h1>
+            {loading ? <h1>Loading...</h1>:
+            <div>
+                <h2>{title}</h2>
+                <img src={img} alt={title}/>
+                <ul>
+                    {genres.map(genre => <li key={genre}>{genre}</li>)}
+                </ul>
+                <p>{summary}</p>
+            </div>
+            }
+
+        </div>
     );
 }
 
